77bf8e1555cd887f2a911b2207810177d5b9382f
Yoshidak-ta <bsk.gooserock@gmail.com>
2024-05-14 07:23:28 +0900

    現状コード共有

--- nagoyameshi/src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
@@ -18,10 +18,10 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests((requests) -> requests
-						.requestMatchers("/css/**", "/images/**", "/js/**", "/", "/users/**",
+						.requestMatchers("/css/**", "/images/**", "/js/**", "/",
 								"/signup/**",
 								"/stores/",
-								"/stores/{id}", "/stripe/webhook", "/stores/{id}/reviews")
+								"/stores/{id}", "/stripe/webhook", "/stores/{id}/reviews", "/stores/{id}/menus")
 						.permitAll() // すべてのユーザーにアクセスを許可するURL           
 						.requestMatchers("/admin/**").hasRole("ADMIN") // 管理者にのみアクセスを許可するURL
 						.anyRequest().authenticated() // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
